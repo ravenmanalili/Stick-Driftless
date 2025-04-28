86ae1b03a9172e40e6d4673c1c4248bd1d300068
SenseiRands <senseirnd04@gmail.com>
2025-04-28 13:16:57 +0800

    feat(product-details.blade.php)
    Each gamepad now have their respective descriptions

--- src/stick-driftless/resources/views/product-details.blade.php
+++ src/stick-driftless/resources/views/product-details.blade.php
@@ -12,7 +12,7 @@
   <div class="flex flex-col gap-y-12 text-start max-md:px-6">
     <h1 class="text-6xl font-bold">{{ $gamepad->gamepad_name }}</h1>
     <h2 class="text-5xl font-semibold">${{ number_format($gamepad->price, 2) }}</h2>
-    <p class="max-w-lg text-xl">{{ $gamepad->description ?? 'The custom ' . $gamepad->platform . ' controller designed to help you react faster & win more. Over 8 Hours of Active Gameplay. Pairs Well With Shooter, Action and Adventure Games.' }}</p>
+    <p class="max-w-lg text-xl">{{ $gamepad->gamepad_description ?? 'The custom ' . $gamepad->platform . ' controller designed to help you react faster & win more. Over 8 Hours of Active Gameplay. Pairs Well With Shooter, Action and Adventure Games.' }}</p>
     <div class="flex flex-row gap-x-4 max-sm:flex-col max-sm:gap-y-4">
         <button type="" class="flex justify-center p-3 text-2xl font-extrabold text-gray-100 transition duration-500 transform rounded-lg cursor-pointer h-fit w-52 bg-gradient-to-r from-blue-700 to-blue-500 hover:scale-110 dark:bg-gradient-to-r dark:from-blue-950 dark:to-blue-900 max-sm:w-64">Order Now</button>
         <button type="" class="flex justify-center p-3 text-2xl font-extrabold text-gray-100 transition duration-500 transform rounded-lg cursor-pointer h-fit w-52 bg-gradient-to-r from-blue-700 to-blue-500 hover:scale-110 dark:bg-gradient-to-r dark:from-blue-950 dark:to-blue-900 max-sm:w-64">Add to Cart</button>
